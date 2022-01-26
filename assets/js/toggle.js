a0e5607a7fef3e717408ce9d5a0553351a3f523a
Cody Michaud <cody.r.michaud@gmail.com>
2022-01-26 15:08:47 -0600

    Finished content and styling. moving to toggle functionality

---
+++ assets/js/toggle.js
@@ -0,0 +1,5 @@
+//Creating variables for query selectors
+const toggleBtn = document.querySelector('#toggle');
+const title = document.querySelector('#title');
+const cssFile = document.querySelector('#cssFile');
+const prompt = document.querySelector('#prompt');
