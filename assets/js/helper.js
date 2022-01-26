7dc30e47ae9cbb90053091568b893039a8e62c19
Cody Michaud <cody.r.michaud@gmail.com>
2022-01-26 17:03:54 -0600

    Working through helper.js

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,1 +1,18 @@
 //Formatting Response
+const wordResponse = (res) => {
+    if (!res) {
+        console.log(res.status);
+    }
+    if (!res.length) {
+        responseField.innerHTML = '<p>Try again!</p><p>We could not find any suggestions';
+        return;
+    }
+    //Array for the strings
+    let wordArr = [];
+
+    for (let i = 0; i < Math.min(res.length, 10); i++) {
+        wordArr.push(`<li>${res[i].word}`);
+    }
+
+    wordArr = wordArr.join("");
+}
