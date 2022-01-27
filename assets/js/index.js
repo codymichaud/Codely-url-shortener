283a03ca88b2b337c0aa424a9f40a07f56750c53
Cody Michaud <cody.r.michaud@gmail.com>
2022-01-27 13:50:43 -0600

    Finished url shortening functionality. Now working through debugging and continuing to style and add content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,38 @@
-const apiUrl = 'https://api.datamuse.com/words?';
-const query = 'rel_jjb=';
-
-const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#response');
+const inputField = document.querySelector('#input');
+const apiKey = '82f3c82177aa447bb9e71bb1f2bb58ee';
+const rebrandlyUrl = 'https://api.rebrandly.com/v1/links';
 
+const shortenBtn = document.querySelector('#shorten');
+
+const shortenUrl = async () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten });
+
+    try {
+        const response = await fetch(rebrandlyUrl, {
+            method: 'POST',
+            body: data,
+            headers: {
+                'Content-type': 'application/json',
+                'apikey': apiKey
+            }
+        })
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            byteResponse(jsonResponse);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
+    }
+    shortenUrl();
+}
+
+shortenBtn.addEventListener('click', displayShortUrl);
