ede08aa236b290ca1024be28e265c7e0369b7e67
Cody Michaud <cody.r.michaud@gmail.com>
2022-01-27 11:45:31 -0600

    Declaring vars for grabbing ids from html

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const apiUrl = 'https://api.datamuse.com/words?';
+const query = 'rel_jjb=';
+
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#response');
+
